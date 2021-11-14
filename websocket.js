9cbf2c3c8f05961c7d230cc74fba0a62d3a096a6
vaib1343 <vaib1343@gmail.com>
2021-11-14 11:45:22 +0530

    first cmmit

--- websocket.js
+++ websocket.js
@@ -3,55 +3,60 @@ const { processMessage } = require("./utils/message.utiles");
 const http = require("http");
 const jwt = require("jsonwebtoken");
 const {
-  broadcastMessage,
-  clients,
-  retreiveAndSendMessage,
+    broadcastMessage,
+    clients,
+    retreiveAndSendMessage,
 } = require("./utils/ws.utils");
 
-const server = http.createServer();
 const wss = new websocket.Server({
-  noServer: true,
+    noServer: true,
 });
 
 wss.on("connection", (ws) => {
-  clients.push(ws);
+    clients.push(ws);
 
-  ws.on("close", () => {
-    clients.filter(
-      (generalSocket) => generalSocket.connectionID !== ws.connectionID
-    );
-  });
+    ws.on("close", () => {
+        clients.filter(
+            (generalSocket) => generalSocket.connectionID !== ws.connectionID
+        );
+    });
 
-  ws.on("message", (payload) => {
-    var message = processMessage(payload);
-    if (!message) {
-      return;
-    }
-    if (message.intent === "chat") {
-      broadcastMessage(message, ws);
-    } else if (message.intent === "old-message") {
-      const count = message.count;
-      if (!count) return;
-      retreiveAndSendMessage(ws, count);
-    }
-  });
+    ws.on("message", (payload) => {
+        var message = processMessage(payload);
+        if (!message) {
+            return;
+        }
+        if (message.intent === "chat") {
+            broadcastMessage(message, ws);
+        } else if (message.intent === "old-message") {
+            const count = message.count;
+            if (!count) return;
+            retreiveAndSendMessage(ws, count);
+        }
+    });
 });
+const setwebsocketServer = (app) => {
+    const port = process.env.PORT || 3000;
+    let server = http
+        .createServer(app)
+        .listen(port, console.log.bind(console, "server started"));
 
-server.listen(1337);
+    server.on("upgrade", (request, socket, head) => {
+        const token = request.url.slice(1);
+        var payload;
+        try {
+            payload = jwt.verify(token, process.env.SECRET_TOKEN);
+        } catch (err) {
+            socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+            socket.destroy();
+            return;
+        }
 
-server.on("upgrade", (request, socket, head) => {
-  const token = request.url.slice(1);
-  var payload;
-  try {
-    payload = jwt.verify(token, process.env.SECRET_TOKEN);
-  } catch (err) {
-    socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
-    socket.destroy();
-    return;
-  }
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+            ws.connectionID = payload.email;
+            wss.emit("connection", ws, request);
+        });
+    });
+};
 
-  wss.handleUpgrade(request, socket, head, function done(ws) {
-    ws.connectionID = payload.email;
-    wss.emit("connection", ws, request);
-  });
-});
+module.exports = setwebsocketServer;
